515a0379b90894e5ec5695d5e74212e42a6a99d4
YuliyaVB <ju7iab@gmail.com>
2024-02-06 20:59:39 +0300

    initial commit

---
+++ src/test/java/ru/geekbrains/accessApp/AccessApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.accessApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
